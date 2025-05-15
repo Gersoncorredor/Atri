d12bfb1756a07f2a5e5e147cbec9d2c4e12bee33
gerson <Jp1838883@gmail.com>
2025-05-15 12:06:16 -0500

    feat: add calculator core

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { CalculatorService } from './Calculator.Api/Service/CalculatorService';
 import { CalculationController } from './Calculator.Api/Controllers/CalculatorController';
 
