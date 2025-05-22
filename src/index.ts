e74ad530cca65f24c79cbcc5be8667486142812d
gerson <163358849+Gersoncorredor@users.noreply.github.com>
2025-05-22 17:46:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ app.use(express.json());
 const calculatorService = new CalculatorService();
 const calculationController = new CalculationController(calculatorService);
 
+app.get('/', (req, res) => {
+  res.send('API está corriendo correctamente');
+});
+
 
 app.post('/calculate', calculationController.calculate.bind(calculationController));
 
