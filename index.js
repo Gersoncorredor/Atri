fd906d68df7f3778b59cd62ae39be5dca9f6c144
gerson <Jp1838883@gmail.com>
2025-05-24 19:32:30 -0500

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const path = require("path");
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
